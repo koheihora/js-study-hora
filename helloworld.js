1905eb87d1352781c0bad5e22202cca99163c768
ia04081 <hibic@icloud.com>
2014-06-15 11:50:07 +0000

    Randamize title and content

--- helloworld.js
+++ helloworld.js
@@ -7,11 +7,18 @@ server.listen(process.env.PORT, process.env.IP);
 console.log('Server Running');
 
 //リクエストの処理 今度はHTMLファイルを読み込んで表示させる
+//さらに、ランダムでタイトルと表示の一部を変える
 function doRequest(req, res){
+  var number = Math.floor(Math.random() * 3);
   fs.readFile('./hello.html', 'UTF-8',
               function(err, data){
+                var title = ["ページA","ページB","ページC"];
+                var content = ["This is Sample version","Here is one more content","This is last one"];
+                var data2 = data.
+                replace(/@title@/g, title[number]).
+                replace(/@content@/g, content[number]);
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(data);
+  res.write(data2);
   res.end();
               });
 }
