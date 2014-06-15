6f32f8a87a660600d4ea7b8e39a03264676634f8
ia04081 <hibic@icloud.com>
2014-06-15 09:10:48 +0000

    Read HTML file.

--- helloworld.js
+++ helloworld.js
@@ -1,13 +1,17 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer();
 server.on('request', doRequest);
 server.listen(process.env.PORT, process.env.IP);
 console.log('Server Running');
 
-//リクエストの処理
+//リクエストの処理 今度はHTMLファイルを読み込んで表示させる
 function doRequest(req, res){
+  fs.readFile('./hello.html', 'UTF-8',
+              function(err, data){
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello World');
   res.end();
+              });
 }
